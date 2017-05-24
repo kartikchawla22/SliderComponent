259f9573609f0ed3d8110e7fa2066db8bab98d06
kartik <kartik.chawla@kelltontech.com>
2017-05-24 18:45:10 +0530

    plus minus

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -9,34 +9,34 @@ export class SliderComponent implements OnInit {
   constructor() {
     this.items.length = 22;
    }
-step=1;
+step=2;
 items = [];
   increase(slid, slidval) {
     if (slid.value >= 90) {
-      this.step =5;
-      slid.value += 5;
+      this.step = 5;
     } 
     else
-    {this.step = 1;
-      slid.value++;
+    {this.step = 2;
     }
+    slid.value++;
     slidval.value = slid.value;
-    console.log(this.items.length);
+    // console.log(this.items.length);
   }
 
 
   decrease(slid, slidval) {
-    if (slid.value > 90) {
-      slid.value -= 5;
+   if (slid.value >= 90) {
       this.step =5;
-    }
+    } 
     else
-    {
-      this.step = 1;
-      slid.value--;
+    {this.step = 2;
     }
+ 
+  slid.value--;
+  // console.log(slid.value);
     slidval.value = slid.value;
   }
+
 
 
   ngOnInit() {
