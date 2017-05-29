38b564144590f7dc28ff353fb226bb4aac11ec32
kartik <kartik.chawla@kelltontech.com>
2017-05-29 08:41:52 +0530

    validations

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,13 +8,15 @@ import {Router} from '@angular/router';
 })
 // interface data {username, password};
 export class LoginComponent {
-  data = {username : "", password : ""};
+  data = {email : "", password : ""};
   role = "SA"
+EMAIL_REGEXP = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 constructor(private _router : Router){}
+
 onformsubmit(){
   
-  if(this.data.username == "kartik" && this.data.password == "kartik"){
-    var key = btoa(btoa(this.data.username) + "??" + btoa(this.data.password)  + "??" + btoa(this.role))  ; 
+  if(this.data.email == "kartik" && this.data.password == "kartik"){
+    var key = btoa(btoa(this.data.email) + "??" + btoa(this.data.password)  + "??" + btoa(this.role))  ; 
   document.cookie = "SessionId" + "=" + key;
     this._router.navigate(['/']);
     console.log(key);
@@ -22,7 +24,7 @@ onformsubmit(){
   else{
     alert('Wrong username/password');
     this._router.navigate(['/login']);
-    this.data.username = "";
+    this.data.email = "";
     this.data.password = "";
     console.log("eh")
     console.log(key);
